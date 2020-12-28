b0b91ac34165e7b61451816a7c6b727fb6489ef4
Elizaveta Trushnikova <etsatsko@mail.ru>
2020-12-28 15:21:50 +0500

    slider

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -78,6 +78,45 @@ window.onload=function(){
 	});
 
 	/* Слайдер */
+	let slideIndex = 1;
 
+	/* Устанавливает текущий слайд */
+	function currentSlide(n) {
+	    showSlides(slideIndex = n);
+	}
+
+	/* Бесконечный слайдер */
+	setInterval(function(){
+		var dots = document.getElementsByClassName("dot");
+		for (i = 0; i < dots.length; i++) {
+	        if(dots[i].style.className == "dot active")
+	        	slideIndex = dots[i];
+	    }
+	    slideIndex++;
+	    showSlides(slideIndex);
+	}, 5000);
+
+	/* Основная функция слайдера */
+	function showSlides(n) {
+	    var i;
+	    var slides = document.getElementsByClassName("slides");
+	    var dots = document.getElementsByClassName("dot");
+
+	    if (n > slides.length) {
+	      slideIndex = 1
+	    }
+	    else if (n < 1) {
+	        slideIndex = slides.length;
+	    }
+	    for (i = 0; i < slides.length; i++) {
+	        slides[i].style.display = "none";
+	    }
+	    for (i = 0; i < dots.length; i++) {
+	        dots[i].className = dots[i].className.replace(" active", "");
+	    }
+
+	    	slides[slideIndex-1].style.display = "block";
+	    	dots[slideIndex-1].className += " active";  
+	}
 	
 }
